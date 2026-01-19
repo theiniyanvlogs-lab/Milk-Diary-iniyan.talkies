4d8e66ddb5e70f87342dc101866c3b33ab4c5649
theiniyanvlogs-lab <the.iniyan.vlogs@gmail.com>
2026-01-19 12:24:14 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "milkdiary-cache-v3";
+const CACHE_NAME = "milkdiary-cache-v4";
 const urlsToCache = ["./", "./index.html", "./manifest.json", "./192.png", "./512.png"];
 
 self.addEventListener("install", event => {
